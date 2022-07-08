2cf5b02014b53de3b7ca6a48ade6c6d53a691919
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-07-08 11:05:39 +0300

    Add products form and list

---
+++ src/pages/productForm/productForm.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+
+const ProductForm = () => {
+  return (
+    // <nav-bar></nav-bar>
+    <React.Fragment>
+      <Navbar />
+      <form className="form">
+        <h2 className="form-title">Add Product</h2>
+        <div className="form-border">
+          <input
+            className="form-control"
+            name="Title"
+            type="text"
+            placeholder="Title"
+            autoComplete="false"
+          />
+          <input
+            className="form-control"
+            name="Price"
+            type="text"
+            placeholder="Price"
+          />
+          <label for="Category">Categories :</label>
+          <select
+            id="Category"
+            className="form-control"
+            style={{ marginBottom: "20px" }}
+            name="CategoryId"
+          >
+            <option value=""></option>
+          </select>
+          <input
+            className="form-control"
+            name="ImageUrl"
+            type="text"
+            autocomplete="off"
+            placeholder="ImageUrl"
+          />
+          <input
+            className="btn btn-danger  btn-block"
+            type="submit"
+            value="Insert"
+          />
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default ProductForm;
---
+++ src/pages/productList/productList.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import Background from "../../assets/img/back3.jpg";
+// import { connect } from "react-redux";
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+class ProductList extends Component {
+  render() {
+    // const { products } = this.props;
+    const productList = [].length ? (
+      [].map((product: any) => {
+        return (
+          <div
+            className="list-image col-lg-3 col-md-4 col-sm-6"
+            key={product.productId}
+          >
+            <Image
+              className="show list"
+              // the way of access to the public folder url not to use directly
+              src={process.env.PUBLIC_URL + product.imageUrl}
+              alt={product.title}
+            />
+            <br />
+            <span className="name"> {product.title} </span>
+            <h4 className="price btn btn-danger active">{product.price}</h4>
+            <Link href="" passHref>
+              <p className="click btn btn-info">Click to get</p>
+            </Link>
+          </div>
+        );
+      })
+    ) : (
+      <div> There&apos;s no products yet </div>
+    );
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="wrapper">
+          <Sidebar />
+          <div className="product-bg" id="content">
+            <div id="content">
+              <div className="row">
+                <div className="back col-sm-12">
+                  <Image src={Background} />
+                </div>
+                {productList}
+              </div>
+              <br />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => {
+//   return {
+//     products: state.products,
+//   };
+// };
+
+export default ProductList; // connect(mapStateToProps)(ProductList);
