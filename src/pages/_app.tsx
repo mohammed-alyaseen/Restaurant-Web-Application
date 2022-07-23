4750eacb4687405b771b99ab5bc75084c9abf100
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-07-23 15:13:00 +0300

    Fix typo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/font-awesome/css/font-awesome.min.css";
-import "../style/globals.css";
+import "../styles/globals.css";
 
 interface IPropMyApp {
   Component: any;
