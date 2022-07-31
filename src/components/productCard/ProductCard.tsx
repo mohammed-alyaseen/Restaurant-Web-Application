3c834777b832e40c1d0c20bae51dfd23edc1af9b
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-07-31 09:35:19 +0300

    Implement new card layout & style

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../../styles/productCard.module.css";
 
 interface IProductCardProp {
   product: any;
@@ -8,7 +9,7 @@ interface IProductCardProp {
 function ProductCard({ product }: IProductCardProp) {
   return (
     <>
-      <Image
+      {/* <Image
         className="show list"
         src={product.imageUrl}
         alt={product.title}
@@ -20,7 +21,74 @@ function ProductCard({ product }: IProductCardProp) {
       <h4 className="price-btn btn btn-danger active">{product.price}</h4>
       <Link href="" passHref>
         <p className="click btn btn-info">Click to get</p>
-      </Link>
+      </Link> */}
+      {/* <div className="col-sm-6 col-md-6 col-lg-4"> */}
+      <div className={styles.foodCard}>
+        <div className={styles.foodCard_img}>
+          <Image
+            src={product.imageUrl}
+            alt={product.title}
+            width={300}
+            height={250}
+          />
+          <Link href="#!" passHref>
+            <i className="far fa-heart"></i>
+          </Link>
+        </div>
+        <div className={styles.foodCard_content}>
+          <div className={styles.foodCard_titleSection}>
+            <Link href="#!">{product.title}</Link>
+            {/* <Link href="#!" className={styles.foodCard_author}>
+              Burger
+            </Link> */}
+          </div>
+          <div className={styles.foodCard_bottomSection}>
+            <div className={styles.spaceBetween}>
+              {/* <div>
+                <span className="fa fa-fire"></span> 220 - 280 Kcal
+              </div> */}
+              {/* <div className="pull-right">
+                <span className="badge badge-success">Veg</span>
+              </div> */}
+            </div>
+            <hr />
+            <div className={styles.spaceBetween}>
+              <div className={styles.foodCard_price}>{product.price}$</div>
+              <div className={styles.foodCard_orderCount}>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <button
+                      className="btn btn-outline-secondary minus-btn"
+                      type="button"
+                      id="button-addon1"
+                    >
+                      <i className="fa fa-minus"></i>
+                    </button>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control input-manulator"
+                    placeholder=""
+                    aria-label="Example text with button addon"
+                    aria-describedby="button-addon1"
+                    value="0"
+                  />
+                  <div className="input-group-append">
+                    <button
+                      className="btn btn-outline-secondary add-btn"
+                      type="button"
+                      id="button-addon1"
+                    >
+                      <i className="fa fa-plus"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
     </>
   );
 }
